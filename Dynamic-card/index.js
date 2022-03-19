937c3fc8256a2106c6d8f4f323cf71975a076492
Asif Ahmed <asifbin8@gmail.com>
2022-03-20 00:40:55 +0600

    commit

---
+++ Dynamic-card/index.js
@@ -0,0 +1,27 @@
+//dummy data//
+
+const posts=[
+    {
+        title :"this is title1";
+        body :"this is body 1"
+    }
+    
+    {
+        title:"this is title1";
+        body:"this is body 1"
+    }
+    
+    {
+        title:"this is title1";
+        body:"this is body 1"
+    }
+    
+    {
+        title:"this is title1";
+        body:"this is body 1"
+    }]
+
+    //selection//
+
+    
+
